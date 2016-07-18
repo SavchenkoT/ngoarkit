48a0d9ef61a345efea575c62a3335ce3ae9eff51
polina <tanja-asf@ukr.net>
2016-07-18 17:53:18 +0300

    fix

--- app/scripts/modules/M02-login.js
+++ app/scripts/modules/M02-login.js
@@ -3,8 +3,27 @@ window.$ = window.jQuery = require('jquery');
 
 module.exports = function(vars) {
 
-  $("#authpage").each(function(){
-		console.log('%c hello M02-login init! ', 'background: #A3C5E9; color: white');
-  });
+$(function() {
+
+    $('#login-form-link').click(function(e) {
+		$("#login-form").delay(100).fadeIn(100);
+ 		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('#register-form-link').click(function(e) {
+		$("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
+});
+
+//  $("#authpage").each(function(){
+//		console.log('%c hello M02-login init! ', 'background: #A3C5E9; color: white');
+//  });
 
 };
