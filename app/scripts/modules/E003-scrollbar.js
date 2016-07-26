30c2032c57bbb349a0c1eb04ed56fa29d494e79e
polina <tanja-asf@ukr.net>
2016-07-26 17:46:19 +0300

    inspection

--- app/scripts/modules/E003-scrollbar.js
+++ app/scripts/modules/E003-scrollbar.js
@@ -25,15 +25,21 @@ module.exports = function(vars) {
 
 };
 
-$(document).on('click', '.panel-heading span.clickable', function(e){
-    var $this = $(this);
-	if(!$this.hasClass('panel-collapsed')) {
-		$this.parents('.panel').find('.panel-body').slideUp();
-		$this.addClass('panel-collapsed');
-		$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
-	} else {
-		$this.parents('.panel').find('.panel-body').slideDown();
-		$this.removeClass('panel-collapsed');
-		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-	}
-})
+	$(".clickable").on("click", function(){
+	 var $this = $(this)
+	 $this.parent().parent().find('.panel-body').toggleClass("open");
+	 $this.find('i.fa').toggleClass("fa-chevron-down").toggleClass("fa-chevron-up");
+	});
+
+// $(document).on('click', '.panel-heading span.clickable', function(e){
+//     var $this = $(this);
+// 	if(!$this.hasClass('panel-collapsed')) {
+// 		$this.parents('.panel').find('.panel-body').slideUp();
+// 		$this.addClass('panel-collapsed');
+// 		$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+// 	} else {
+// 		$this.parents('.panel').find('.panel-body').slideDown();
+// 		$this.removeClass('panel-collapsed');
+// 		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+// 	}
+// })
