79aaf4672e05d64be1b8a4e7895dfe1076449596
tanja-asf@ukr.net <tanja20124@gmail.com>
2016-07-26 12:34:11 +0300

    logo,register, filters

--- app/scripts/libs/panels.js
+++ app/scripts/libs/panels.js
@@ -1,5 +1,11 @@
 var $copyDiv = $(".panel-default").find('.panel-heading').first().clone();
 var $copyColap = $(".panel-default").find('.panel-collapse').first().clone();
 $("#plusToPanel").on('click', function(){
-        $copyDiv.insertAfter("#collapseOne").after($copyColap);
+  $copyDiv.insertAfter("#collapseOne").after($copyColap);
 });
+
+
+$('#myTab a').click(function (e) {
+  e.preventDefault();
+  $(this).tab('show');
+})
--- app/scripts/modules/M02-login.js
+++ app/scripts/modules/M02-login.js
@@ -3,27 +3,23 @@ window.$ = window.jQuery = require('jquery');
 
 module.exports = function(vars) {
 
-$(function() {
+  $(function() {
 
-    $('#login-form-link').click(function(e) {
-		$("#login-form").delay(100).fadeIn(100);
- 		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-	$('#register-form-link').click(function(e) {
-		$("#register-form").delay(100).fadeIn(100);
- 		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
+      $('#login-form-link').click(function(e) {
+  		$("#login-form").delay(100).fadeIn(100);
+   		$("#register-form").fadeOut(100);
+  		$('#register-form-link').removeClass('active');
+  		$(this).addClass('active');
+  		e.preventDefault();
+  	});
+  	$('#register-form-link').click(function(e) {
+  		$("#register-form").delay(100).fadeIn(100);
+   		$("#login-form").fadeOut(100);
+  		$('#login-form-link').removeClass('active');
+  		$(this).addClass('active');
+  		e.preventDefault();
+  	});
 
-});
-
-//  $("#authpage").each(function(){
-//		console.log('%c hello M02-login init! ', 'background: #A3C5E9; color: white');
-//  });
+  });
 
 };
