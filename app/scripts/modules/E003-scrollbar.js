4c9640d9338abbb25ffdeafdb5f6d10bbcc983fd
SavchenkoT <tsavchenko@ngoar.net>
2017-07-14 15:46:23 +0300

    commit-1

--- app/scripts/modules/E003-scrollbar.js
+++ app/scripts/modules/E003-scrollbar.js
@@ -30,16 +30,3 @@ module.exports = function(vars) {
 	 $this.parent().parent().parent().find('.panel-body').toggleClass("open");
 	 $this.find('i.fa').toggleClass("fa-chevron-down").toggleClass("fa-chevron-up");
 	});
-
-// $(document).on('click', '.panel-heading span.clickable', function(e){
-//     var $this = $(this);
-// 	if(!$this.hasClass('panel-collapsed')) {
-// 		$this.parents('.panel').find('.panel-body').slideUp();
-// 		$this.addClass('panel-collapsed');
-// 		$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
-// 	} else {
-// 		$this.parents('.panel').find('.panel-body').slideDown();
-// 		$this.removeClass('panel-collapsed');
-// 		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-// 	}
-// })
