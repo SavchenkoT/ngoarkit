6f0a5b7973a5518cf30fbd9895101cc8b92f4df9
SavchenkoT <tsavchenko@ngoar.net>
2017-08-08 23:47:05 +0300

    add-inspection

--- app/scripts/libs/proba.js
+++ app/scripts/libs/proba.js
@@ -15,3 +15,18 @@ $(document).on('click', '.panel-heading span.clickable', function(e){
 		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
 	}
 })
+
+// fa-ellipsis-v
+
+$(document).on('click', '.panel-heading .clickable', function(e){
+    var $this = $(this);
+	if(!$this.hasClass('panel-collapsed')) {
+		$this.parents('.panel').find('.panel-body').slideUp();
+		$this.addClass('panel-collapsed');
+		$this.find('i').removeClass('glyphicon-chevron-up').addClass('fa-ellipsis-v');
+	} else {
+		$this.parents('.panel').find('.panel-body').slideDown();
+		$this.removeClass('panel-collapsed');
+		$this.find('i').removeClass('fa-ellipsis-v').addClass('fa-ellipsis-v');
+	}
+})
